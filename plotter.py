0c24fab1e9f8ca04d120d841ddea8f10fba13107
Parth Paradkar <parthparadkar3@gmail.com>
2019-03-04 10:46:45 +0530

    Working

---
+++ plotter.py
@@ -0,0 +1,29 @@
+import script
+import sys
+
+
+def main():
+    arg = sys.argv[1:]
+    x_scale = float(arg[0])
+    y_scale = float(arg[1])
+    script.Point.set_scales(x_scale, y_scale)
+    file_path = arg[2]
+    pt_list = []
+    with open(file_path, 'r') as f:
+        for line in f:
+            temp_x, temp_y = line.split(',')
+            pt_list.append(('({},{})'.format(temp_x, temp_y)))
+
+    points = script.Pointset(pt_list)
+    print('On X axis 1mm = {} units\nOn Y axis 1mm = {} units\n'.format(script.Point.x_scale, script.Point.y_scale))
+    for point in points.point_list:
+        print(point, '\n{} lines after {} on X axis \n{} lines above {} on Y axis\n'.format(point.x_line, point.x_prev, point.y_line, point.y_prev))
+
+
+main()
+
+'''
+temp_x, temp_y = line.split(',')
+temp_points.append(('({},{})'.format(temp_x, temp_y)))
+
+'''
